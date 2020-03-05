52062f0efad2a30a78d49f5b9c9b3063b86494e6
RobvN <robertvn944@gmail.com>
2020-03-05 04:37:55 +0200

    Dev branch

--- src/test/java/com/acme/APITest.java
+++ src/test/java/com/acme/APITest.java
@@ -7,19 +7,22 @@ import org.testng.annotations.Test;
 
 public class APITest {
 	
+	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.s");
+	
 	@Test
 	public void postJira() {
 		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.s");
 		String date = simpleDateFormat.format(new Date());
 		System.out.println("I have posted JIRA code");
 		System.out.println("This change has been posted from the original MavenJava project in GITstuff");
-		System.out.println(date + "::GITercise - And I have tidied up your timeline and added this code");
+		System.out.println(date + " :: GITercise - And I have tidied up your timeline and added this code");
 	}
 	
 	@Test(groups= {"smoke"})
 	public void deleteTwitter() {
 		
+		String anotherDate = simpleDateFormat.format(new Date());
 		System.out.println("I have deleted Twitter");
+		System.out.println(anotherDate + " :: Added date formatting");
 	}
 }
